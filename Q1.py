15978d831b4c3b09f75257e211a4d9337c59d179
jakeperalta <Sushmina.Dhakal@triosstudent.com>
2023-03-31 09:14:36 -0400

    made some changes again

--- Q1.py
+++ Q1.py
@@ -3,3 +3,15 @@ x= input("Enter patient's name:")
 q1= input("Was clesning performed? y/n :")
 q2= input("Was cavity-filling performed? y/n :")
 q3= input("Was X-ray performed? y/n :")
+
+print("1- Cleaning rate: $60")
+print("2- Cavity filling rate: $200")
+print("3- X-Ray rate: $100")
+print("4- Tax rate: 15%")
+
+print("Tax")
+def calculating_tax(a,b,c):
+  e=a+b+c
+  e=e*.15
+  print(e)
+t=calculating_tax(60,200,100)
